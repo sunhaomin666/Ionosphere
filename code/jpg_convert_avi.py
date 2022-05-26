2828384a370a4e5eb2ee335e333262d2870d843a
sunhaomin666 <48083665+sunhaomin666@users.noreply.github.com>
2022-05-26 15:11:11 +0800

    Update jpg_convert_avi.py

--- code/jpg_convert_avi.py
+++ code/jpg_convert_avi.py
@@ -5,7 +5,7 @@ fps =2 #保存视频的FPS，可以适当调整
 size=(380,369)
 
 fourcc=cv2.VideoWriter_fourcc(*'XVID')
-videowriter=cv2.VideoWriter('D:/3.avi',fourcc,fps,size)
+videowriter=cv2.VideoWriter('D:/tec.avi',fourcc,fps,size)
 
 for i in range(0,24):
     frame =cv2.imread(img_root+str(i)+'.jpg')
